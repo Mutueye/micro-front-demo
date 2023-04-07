fae13c3733a1801196fe7e31c2fc22cf443bc6fd
Mutueye <mutuyueniao@gmail.com>
2023-04-07 17:33:47 +0800

    style: athena-main

--- packages/athena-main/src/modules/home/views/Home.vue
+++ packages/athena-main/src/modules/home/views/Home.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="w-full flex flex-col items-center">
     <Banner />
-    <!-- <Example /> -->
+    <Example />
   </div>
 </template>
 
 <script lang="ts" setup>
   import Banner from './components/Banner.vue';
-  // import Example from './components/Example.vue';
+  import Example from './components/Example.vue';
 </script>
