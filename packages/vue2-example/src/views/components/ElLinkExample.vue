c21c61a22bf2701b60a1e6414a1631c661d642c3
Mutueye <mutuyueniao@gmail.com>
2023-04-19 10:21:09 +0800

    style: element-ui css override WIP

---
+++ packages/vue2-example/src/views/components/ElLinkExample.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="mt-space-xxxl">
+    <h1 class="mt-space-xl">文字链接</h1>
+    <div class="mt-space-xs">
+      <el-link href="https://element.eleme.io" target="_blank">默认链接</el-link>
+      <el-link type="primary">主要链接</el-link>
+      <el-link type="success">成功链接</el-link>
+      <el-link type="warning">警告链接</el-link>
+      <el-link type="danger">危险链接</el-link>
+      <el-link type="info">信息链接</el-link>
+    </div>
+    <div class="mt-space-xs">
+      <el-link disabled>默认链接</el-link>
+      <el-link type="primary" disabled>主要链接</el-link>
+      <el-link type="success" disabled>成功链接</el-link>
+      <el-link type="warning" disabled>警告链接</el-link>
+      <el-link type="danger" disabled>危险链接</el-link>
+      <el-link type="info" disabled>信息链接</el-link>
+    </div>
+  </div>
+</template>
---
+++ packages/vue2-example/src/views/components/ElRadioExample.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="mt-space-xxxl">
+    <h1 class="mt-space-xl">单选框</h1>
+    <div class="mt-space-xs">
+      <el-radio v-model="radio" label="1">备选项</el-radio>
+      <el-radio v-model="radio" label="2">备选项</el-radio>
+    </div>
+    <div class="mt-space-xs">
+      <el-radio disabled v-model="radio" label="1">备选项</el-radio>
+      <el-radio disabled v-model="radio" label="2">备选项</el-radio>
+    </div>
+    <div class="mt-space-xs">
+      <el-radio-group v-model="radio1">
+        <el-radio-button label="上海"></el-radio-button>
+        <el-radio-button label="北京"></el-radio-button>
+        <el-radio-button label="广州"></el-radio-button>
+        <el-radio-button label="深圳"></el-radio-button>
+      </el-radio-group>
+    </div>
+    <div class="mt-space-xs">
+      <el-radio-group v-model="radio2" size="medium">
+        <el-radio-button label="上海"></el-radio-button>
+        <el-radio-button label="北京"></el-radio-button>
+        <el-radio-button label="广州"></el-radio-button>
+        <el-radio-button label="深圳"></el-radio-button>
+      </el-radio-group>
+    </div>
+    <div class="mt-space-xs">
+      <el-radio-group v-model="radio3" size="small">
+        <el-radio-button label="上海"></el-radio-button>
+        <el-radio-button label="北京" disabled></el-radio-button>
+        <el-radio-button label="广州"></el-radio-button>
+        <el-radio-button label="深圳"></el-radio-button>
+      </el-radio-group>
+    </div>
+    <div class="mt-space-xs">
+      <el-radio-group v-model="radio4" disabled size="mini">
+        <el-radio-button label="上海"></el-radio-button>
+        <el-radio-button label="北京"></el-radio-button>
+        <el-radio-button label="广州"></el-radio-button>
+        <el-radio-button label="深圳"></el-radio-button>
+      </el-radio-group>
+    </div>
+    <div class="mt-space-xs">
+      <el-radio-group v-model="radio1">
+        <el-radio-button label="上海"></el-radio-button>
+        <el-radio-button label="北京"></el-radio-button>
+        <el-radio-button label="广州"></el-radio-button>
+        <el-radio-button label="深圳"></el-radio-button>
+      </el-radio-group>
+    </div>
+    <div class="mt-space-xs">
+      <el-radio v-model="radio1" label="1" border>备选项1</el-radio>
+      <el-radio v-model="radio1" label="2" border>备选项2</el-radio>
+    </div>
+    <div class="mt-space-xs">
+      <el-radio v-model="radio2" label="1" border size="medium">备选项1</el-radio>
+      <el-radio v-model="radio2" label="2" border size="medium">备选项2</el-radio>
+    </div>
+    <div class="mt-space-xs">
+      <el-radio-group v-model="radio3" size="small">
+        <el-radio label="1" border>备选项1</el-radio>
+        <el-radio label="2" border disabled>备选项2</el-radio>
+      </el-radio-group>
+    </div>
+    <div class="mt-space-xs">
+      <el-radio-group v-model="radio4" size="mini" disabled>
+        <el-radio label="1" border>备选项1</el-radio>
+        <el-radio label="2" border>备选项2</el-radio>
+      </el-radio-group>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+  export default {
+    data() {
+      return {
+        radio: '1',
+        radio1: '上海',
+        radio2: '上海',
+        radio3: '上海',
+        radio4: '上海',
+        isIndeterminate: true,
+      };
+    },
+  };
+</script>
